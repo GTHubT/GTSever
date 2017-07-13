c3ddb68bb8088588dfadf027e146335efde21706
GT <tgx_gt@163.com>
2017-07-13 18:10:17 +0800

    fix tricky issue

--- src/cpp/GTServer/GTServer/GT_ThreadPool.cpp
+++ src/cpp/GTServer/GTServer/GT_ThreadPool.cpp
@@ -22,15 +22,14 @@ namespace GT {
             }
 			poolsize_ = poolsize;
 			for (int index = 0; index < poolsize; index++) {
-				std::atomic<bool> end_thread(false);
-				std::shared_ptr<Thread_Tuple> thread_tuple(new Thread_Tuple(std::thread(&GT_ThreadPool::LongTimeWorker_, this, thread_func, std::ref(end_thread))));
-				thread_tuple->end_thread_ = std::move(end_thread.load());
-				printf("create thread TID = %d \n", thread_tuple->this_thread_.get_id());
-				workpool_.push_back(thread_tuple);
+				Thread_Tuple* thread_tuple = new Thread_Tuple();
+                thread_tuple->this_thread_ = std::move(std::thread(&GT_ThreadPool::LongTimeWorker_, this, thread_func, std::ref(thread_tuple->end_thread_)));
+                workpool_.push_back(thread_tuple);
+                printf("create thread TID = %d \n", thread_tuple->this_thread_.get_id());
 			}
         }
 
-        void GT_ThreadPool::LongTimeWorker_(std::function<void()> f, std::atomic<bool> end_thread) {
+        void GT_ThreadPool::LongTimeWorker_(std::function<void()> f, std::atomic<bool>& end_thread) {
             while (!end_thread) {
                 f();
             }
@@ -45,6 +44,7 @@ namespace GT {
                 if ((*iter)->this_thread_.joinable()) {
 					(*iter)->this_thread_.join();
                 }
+                delete (*iter);
                 iter++;
             }
             printf("all worker exit! \n");
