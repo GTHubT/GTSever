56bcf9f9eb2262427437d1e8b7e619a6509ca236
GT <tgx_gt@163.com>
2017-08-01 17:53:05 +0800

    add module wrapper for invoke

---
+++ src/cpp/GTServer/GT_Server_Wapper/GT_Module_Wrapper.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "GT_Module_Wrapper.h"
+
+namespace GT {
+    namespace MODULE {
+
+        GT_Module_Wrapper::GT_Module_Wrapper()
+        {
+        }
+
+
+        GT_Module_Wrapper::~GT_Module_Wrapper()
+        {
+        }
+    }
+}
---
+++ src/cpp/GTServer/GT_Server_Wapper/GT_Module_Wrapper.h
@@ -0,0 +1,17 @@
+#ifndef GT_MODULE_WRAPPER_H_
+#define GT_MODULE_WRAPPER_H_
+
+namespace GT {
+    namespace   MODULE {
+
+
+        class GT_Module_Wrapper
+        {
+        public:
+            GT_Module_Wrapper();
+            ~GT_Module_Wrapper();
+        };
+    }
+}
+
+#endif
